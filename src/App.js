8a5fc7089b15d5adb5956b0fcfcfb12e1ca138f1
marcinoles87 <marcinoles87@gmail.com>
2023-04-18 16:20:36 +0200

    work with structure

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 
 import './App.css';
+
+
 import React from 'react'
+import ContactCard from './components/ContactCard';
 
 const App = () => {
 
     return(
-        <div className='contact-card'>
-            <img src='https://via.placeholder.com/150' alt='profil-img'></img>
 
-        <div className='user-details'>
-            <h1>user name</h1>
-            <p>user Info</p>
-            <p>job : </p>
-            <button>User Age</button>
-        </div>
-        </div>
+        <ContactCard></ContactCard>
+       
     )
 }
 
---
+++ src/components/ContactCard.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const ContactCard = () => {
+    return(
+        <div className='contact-card'>
+            <img src='https://via.placeholder.com/150' alt='profil-img'></img>
+
+        <div className='user-details'>
+            <h1>user name</h1>
+            <p>user Info</p>
+            <p>job : </p>
+            <button>User Age</button>
+        </div>
+        </div>
+    )
+}
+
+export default ContactCard
