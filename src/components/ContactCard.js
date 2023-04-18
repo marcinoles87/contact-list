96b59e885a5b6e1ec0f0f701a2d9fa942255ac55
marcinoles87 <marcinoles87@gmail.com>
2023-04-18 16:27:50 +0200

    work with structure

--- src/components/ContactCard.js
+++ src/components/ContactCard.js
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import './contactcard.css'
 
 const ContactCard = () => {
+
+const [age , showAge ] = useState('')
+
     return(
         <div className='contact-card'>
             <img src='https://via.placeholder.com/150' alt='profil-img'></img>
