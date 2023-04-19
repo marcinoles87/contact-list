291bee99d9ccfaaddcadd14847d735baad26e0de
marcinoles87 <marcinoles87@gmail.com>
2023-04-19 19:13:51 +0200

    work with structure

--- src/App.js
+++ src/App.js
@@ -12,24 +12,25 @@ const App = () => {
     fetch('https://randomuser.me/api/?results=5')
     .then(response => response.json())
     .then(data => {
-        console.log(data)})
-
-        setContacts = (data) => {
-
-        }
-
+        
+        setContacts(data) 
     
+        console.log(data.contacts)
+    })
 
+        
     return(
 
         <div className='app'>
 
-        {contacts.map( (contact , index) => {
-        <ContactCard 
-        avatarURL = {contact.picture}
-        name = {contact.name}
+        {contacts.map( (contact,index) => {
+         return (
+        <ContactCard  key = {index}
+        avatarURL = {contact.picture.large}
+        name = {contact.first}
         email = {contact.email}
-        age = {contact.age} />
+        age = {contact.dob.age} />
+            )
 
          } )}
 
